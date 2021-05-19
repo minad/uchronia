d360a6f6b50f1ba19e692a81ce7708ce77e8d870
Daniel Mendler <mail@daniel-mendler.de>
2021-05-19 15:40:26 +0200

    Cleanup and simplify

--- uchronia.el
+++ uchronia.el
@@ -5,7 +5,7 @@
 ;; Created: 2020
 ;; License: GPL-3.0-or-later
 ;; Version: 0.1
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "27.1"))
 ;; Homepage: https://github.com/minad/uchronia
 
 ;; This file is not part of GNU Emacs.
@@ -29,39 +29,23 @@
 
 ;;; Code:
 
-(require 'subr-x)
-(require 'savehist)
+(eval-when-compile (require 'subr-x))
 
 (defgroup uchronia nil
   "Uchronia"
   :group 'convenience
   :prefix "uchronia-")
 
-(defface uchronia-command
-  '((t :inherit font-lock-keyword-face))
-  "Face used to highlight commands in `uchronia-select'.")
-
-(defcustom uchronia-input-commands
-  '(switch-to-buffer)
-  "Commands which should use the input history."
-  :type 'list)
-
-(defcustom uchronia-input-histories
-  '(buffer-name-history)
-  "Histories which should store the input."
-  :type 'list)
-
 (defcustom uchronia-filter
-  "^uchronia-\\|^execute-extended-command$\\|^exit-minibuffer$"
+  '("^uchronia-"
+    execute-extended-command
+    exit-minibuffer)
   "Filter commands from the uchronia command history."
-  :type 'regexp)
+  :type '(repeat regexp))
 
 (defvar uchronia-history nil)
-(add-to-list 'savehist-minibuffer-history-variables 'uchronia-history)
-
 (defvar-local uchronia--last-input nil)
 (defvar-local uchronia--last-command nil)
-(defvar-local uchronia--last-history nil)
 
 ;;;###autoload
 (define-minor-mode uchronia-mode
@@ -81,65 +65,78 @@
 (defun uchronia--minibuffer-setup ()
   "Setup minibuffer hook of uchronia."
   (setq uchronia--last-command this-command)
-  (add-hook 'post-command-hook #'uchronia--record-input nil t))
+  (add-hook 'post-command-hook #'uchronia--record-input nil 'local))
 
 (defun uchronia--minibuffer-exit ()
   "Exit minibuffer hook of uchronia."
-  (unless (eq t minibuffer-history-variable)
-    (let* ((hist minibuffer-history-variable)
-           (cand (minibuffer-contents-no-properties))
-           (cmd uchronia--last-command)
-           (input uchronia--last-input))
-      (run-at-time
-       0 nil
-       (lambda ()
-         (let ((hist-val (symbol-value hist)))
-           ;; Try to detect if minibuffer has been aborted.
-           ;; TODO Is there a better way?
-           ;; TODO Change this such that aborted sessions can also be repeated?
-           (when (string= (car hist-val) cand)
-             ;; Remove selected candidate from history and store input
-             (when (or (memq cmd uchronia-input-commands)
-                       (memq hist uchronia-input-histories))
-               (set hist (cdr hist-val))
-               (add-to-history hist input))
-
-             ;; Store command history
-             (unless (string-match-p uchronia-filter (symbol-name cmd))
-               (let ((elem (cons cmd input)))
-                 (unless (equal elem (car uchronia-history))
-                   (add-to-history 'uchronia-history elem)))))))))))
+  (let* ((cand (minibuffer-contents-no-properties))
+         (cmd uchronia--last-command)
+         (input uchronia--last-input))
+    (run-at-time
+     0 nil
+     (lambda ()
+       ;; Store command history
+       (unless (string-match-p (string-join
+                                (mapcar (lambda (x)
+                                          (if (stringp x)
+                                              x
+                                            (format "\\`%s\\'" x)))
+                                        uchronia-filter)
+                                "\\|")
+                               (symbol-name cmd))
+         (let ((elem (list cmd input cand)))
+           (unless (equal elem (car uchronia-history))
+             (add-to-history 'uchronia-history elem))))))))
 
 (defun uchronia-repeat (elem)
   "Repeat command history ELEM."
   (interactive
-   (list
-    (if-let (elem (car uchronia-history))
-        elem
-      (error "Command history is empty"))))
+   (list (or (car uchronia-history)
+             (user-error "Command history is empty"))))
   ;; This should not happen due to uchronia-filter, but just check to be sure.
   (when (eq (car elem) #'uchronia-repeat)
-    (error "Cannot repeat `uchronia-repeat'"))
-  (unless uchronia-mode (error "Uchronia mode is not enabled"))
+    (user-error "Cannot repeat `uchronia-repeat'"))
+  (unless uchronia-mode
+    (user-error "Uchronia mode is not enabled"))
   (minibuffer-with-setup-hook
-      (lambda () (insert (cdr elem)))
+      (lambda ()
+        (delete-minibuffer-contents)
+        (insert (cadr elem)))
     (command-execute (car elem))))
 
 (defun uchronia-select ()
   "Select from uchronia command history and call `uchronia-repeat'."
   (interactive)
-  (unless uchronia-mode (error "Uchronia mode is not enabled"))
+  (unless uchronia-mode
+    (user-error "Uchronia mode is not enabled"))
   (let ((cands (or
                 (delete-dups
                  (mapcar (lambda (elem)
                            (cons (concat
                                   (propertize (symbol-name (car elem))
-                                              'face 'uchronia-command)
-                                  " " (cdr elem))
+                                              'face 'font-lock-function-name-face)
+                                  (propertize " " 'display '(space :align-to (+ left 40))) ;; TODO compute aligment
+                                  (propertize (string-trim (cadr elem))
+                                              'face 'font-lock-string-face)
+                                  (propertize " " 'display '(space :align-to (+ left 80))) ;; TODO compute aligment
+                                  (propertize (string-trim
+                                               (replace-regexp-in-string "[\x100000-\x10FFFD]*" "" (caddr elem))) ;; TODO consult hack
+                                               'face 'font-lock-comment-face))
                                  elem))
                          uchronia-history))
-                (error "Command history is empty"))))
-    (uchronia-repeat (cdr (assoc (completing-read "History: " cands nil t nil t) cands)))))
+                (user-error "Command history is empty"))))
+    (uchronia-repeat (cdr (assoc (completing-read "History: "
+                                                  (lambda (str pred action)
+                                                    (if (eq action 'metadata)
+                                                        '(metadata (display-sort-function . identity)
+                                                                   (cycle-sort-function . identity))
+                                                      (complete-with-action action cands str pred)))
+                                                  nil t nil t)
+                                 cands)))))
+
+(defvar savehist-minibuffer-history-variables)
+(with-eval-after-load 'savehist
+  (add-to-list 'savehist-minibuffer-history-variables 'uchronia-history))
 
 (provide 'uchronia)
 ;;; uchronia.el ends here
